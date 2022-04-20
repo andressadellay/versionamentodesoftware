e1074e0376a85e3ae3583303d4bbc91b3ee71977
DanielCaumo <dancaumo@gmail.com>
2022-04-20 20:15:17 -0300

    Atualizando Classe Mercado

--- Mercado.java
+++ Mercado.java
@@ -1,6 +1,29 @@
-1. public class Mercado {
-    2. static float preco;
-    3. static int qte;
-    4. private float total;
-    5. }
-    
+public class Mercado {
+    static float preco;
+    static int qte;
+    private static float total;
+
+    public float getPreco(){
+        return preco;
+    }
+
+    public int getQte(){
+        return qte;
+    }
+
+    public float getTotal(){
+        return total;
+    }
+
+    public void setPreco(float preco){
+        this.preco = preco;
+    }
+
+    public void setQte(int qte){
+        this.qte = qte;
+    }
+
+    public void setTotal(float total){
+        this.total = total
+    }
+}
