ab12b4cc09d58cb329c8febe2cb51a2b8a2a9a73
Alexandre Luís <alexandreluisrigotti@gmail.com>
2022-04-22 20:53:14 -0300

    commit referente tarefa 10

--- Mercado.java
+++ Mercado.java
@@ -2,4 +2,25 @@ public class Mercado {
 	static float preco;
 	static int qte;
 	private float total;
+	public float getPreco(){
+		return preco;
+	}
+
+	public int getQte(){
+		return qte;
+	}
+
+	public float getTotal(){
+		return total;
+	}
+
+	public void setPreco(float preco){
+		this.preco = preco;
+	}
+	public void setQte(int qte){
+		this.qte = qte;
+	}
+	public void setTotal(float total){
+		this.total = total;
+	}
 }
