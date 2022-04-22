1daf9a9210725d2b7ddec6ee24dd5822dc16a499
Kelen Vargas <kelenvargas.senac@gmail.com>
2022-04-22 17:50:31 -0300

    adcionando gets e sets dos atributos-linha 7 até  30

--- Mercado.java
+++ Mercado.java
@@ -2,5 +2,30 @@
  static float preco;
  static int qte;
  private float total;
- }
  
+
+    public float getPreco(){
+        return preco;
+    }
+
+    public int getQte(){
+        return qte;
+    }
+
+    public float getTotal(){
+        return total;
+    }
+
+    public void setPreco(float preco){
+        this.preco = preco;
+    }
+
+    public void setQte(int qte){
+        this.qte = qte;
+    }
+
+    public void setTotal(float total){
+        this.total = total;
+ 
+    } 
+}
