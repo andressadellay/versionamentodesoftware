dd806c177f991072e5e1d8e563dfbe9177b012e6
[brunocorrea91] <[vascopoa@gmail.com]>
2022-04-23 18:05:15 -0300

    Commit de tudo

--- Mercado.java
+++ Mercado.java
@@ -2,4 +2,38 @@ public class Mercado {
      static float preco;
      static int qte;
      private float total;
+     public float getPreco() {
+          return preco;
+     }
+
+     public int getQte() {
+          return qte;
+     }
+
+     public float getTotal() {
+          return total;
+     }
+
+     public void setPreco(float preco) {
+          this.preco = preco;
+     }
+
+     public void setQte(int qte) {
+          this.qte = qte;
+     }
+
+     public void setTotal(float total) {
+          this.total = total;
+     }
+     public Mercado(float preco, int qte, float total){
+           this.preco = 5;
+           this.qte = 3;
+           this.total = total;
+           }
+           public static float preco(float preco, int qte, float
+          total){
+           total = preco*qte;
+           return total;
+           }
+          
 }
